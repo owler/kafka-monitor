08d5cb2dbeac14bcda1ac59a58b3ac64e881e59a
owler <owler@tut.by>
2020-10-14 21:59:29 +0300

    static

--- src/main/scala/event/processor/StaticContentProcessor.scala
+++ src/main/scala/event/processor/StaticContentProcessor.scala
@@ -6,22 +6,23 @@ import org.apache.camel.{Exchange, Processor}
 import org.apache.commons.io.IOUtils
 import java.io.IOException
 import java.nio.file.{FileSystems, Files}
+
+import com.typesafe.scalalogging.Logger
 import org.apache.camel.Exchange
+import org.slf4j.LoggerFactory
 
 class StaticContentProcessor extends Processor {
-
+  val log = Logger(LoggerFactory.getLogger(this.getClass))
   override def process(exchange: Exchange): Unit = {
     val in = exchange.getIn
 
     var relativepath = in.getHeader(Exchange.HTTP_PATH, classOf[String]).replaceAll("/+", "/")
     val requestPath = in.getHeader("CamelServletContextPath", classOf[String]) //CamelServletContextPath
-    println("relativepath: " + relativepath)
     if (relativepath.isEmpty || relativepath == "/") relativepath = "index.html"
 
     val formattedPath = String.format("%s/%s", requestPath, relativepath).replaceAll("/+", "/")
-    println(formattedPath)
+    log.debug("trying " + formattedPath)
     val pathStream = this.getClass.getResourceAsStream(formattedPath)
-    println("pathStream: " + pathStream)
     val path = FileSystems.getDefault.getPath(Paths.get(this.getClass.getResource(formattedPath).toURI).toString)
 
     val out = exchange.getOut
