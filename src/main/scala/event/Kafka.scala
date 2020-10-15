1735547d9a9e7a97ff1594bda925d3d8b64c4091
owler <owler@tut.by>
2020-10-15 13:57:13 +0300

    offset < end

--- src/main/scala/event/Kafka.scala
+++ src/main/scala/event/Kafka.scala
@@ -78,7 +78,7 @@ object Kafka {
 
   def getMessage(topic: String, partition: Int, offset: Long, count: Int = 1): Option[List[KMessage[Array[Byte]]]] = {
     repo.get(topic).flatMap(
-      _.metadata.get(partition).flatMap(offsets => if (offsets._1 != offsets._2 && offset <= offsets._2) Some(offset) else None)
+      _.metadata.get(partition).flatMap(offsets => if (offsets._1 != offsets._2 && offset < offsets._2) Some(offset) else None)
     ) map { verifiedOffset =>
       val consumer = createConsumer()
       val tp = new TopicPartition(topic, partition)
