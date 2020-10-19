14f40456a4efd7a97f5b7b4792dfb5d1b365ede3
owler <owler@tut.by>
2020-10-19 15:16:29 +0300

    nohup

--- src/bin/runMain.sh
+++ src/bin/runMain.sh
@@ -2,6 +2,6 @@
 #JAVA_HOME=/data/iris/utils/linux/java/jdk1.8.0_121/
 #export JAVA_HOME
 
-nohup java -Xms64m -Xmx1256m -cp "../conf:../lib/*:../lib/ext/*" event.KafkaMonitor &
+nohup java -Xms128m -Xmx756m -cp "../conf:../lib/*:../lib/ext/*" event.KafkaMonitor > /dev/null 2>&1 &
 
 
