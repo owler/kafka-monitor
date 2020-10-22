e02379c268156bcc967959af5181b0d9e3e93357
owler <owler@tut.by>
2020-10-22 21:19:24 +0300

    java doc

--- src/main/scala/event/json/Model.scala
+++ src/main/scala/event/json/Model.scala
@@ -13,5 +13,6 @@ case class MsgType(msgtype: String) extends Model
 case class Partitions(partitions: List[Partition]) extends Model
 case class Partition(topic: String, partition: Int, start: Long, end: Long) extends Model
 case class KMessages[T](messages: List[T]) extends Model
+// size is original length of raw bytes and msgsize is decoded msg size before truncate
 case class KMessage[T](offset: Long, timestamp: Date, message: T, size: Long, msgtype: String, msgsize: Long) extends Model
 
