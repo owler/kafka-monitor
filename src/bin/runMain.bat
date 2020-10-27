9828d3556284bb30613ec9350964fd063e30dafe
owler <owler@tut.by>
2020-10-27 20:38:58 +0300

    jaas.conf

--- src/bin/runMain.bat
+++ src/bin/runMain.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Main started
 
-java -Xms512m -Xmx1536m -XX:+UseG1GC -Djava.security.auth.login.config=C:\tools\kafkatool2\jaas.conf -cp "../conf;../lib/*;../lib/ext/*" event.KafkaMonitor
+java -Xms512m -Xmx1536m -XX:+UseG1GC -Djava.security.auth.login.config=..\conf\jaas.conf -cp "../conf;../lib/*;../lib/ext/*" event.KafkaMonitor
 exit
