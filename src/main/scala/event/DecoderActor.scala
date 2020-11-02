ca1c742017302762e298eb5e6f6f56700b428165
owler <owler@tut.by>
2020-11-02 16:27:17 +0300

    use Decoder actor to show msg

--- src/main/scala/event/DecoderActor.scala
+++ src/main/scala/event/DecoderActor.scala
@@ -7,12 +7,13 @@ import event.json.KMessage
 class DecoderActor(decoders: Map[String, Decoder]) extends Actor with ActorLogging {
   override def receive: Receive = {
     case (msgType: String, limit: Int, m: KMessage[Array[Byte]]) =>
+      val start = System.currentTimeMillis()
       val decoder = decoders.getOrElse(msgType, decoders("UTF8"))
       val decoded = decode(decoder, m.message, limit)
       val truncStr = if (decoded.bytes.length >= limit)
         """
           |... message truncated""".stripMargin else ""
-      log.debug("decoded " + m.offset)
+      log.debug(s"decoded offset: ${m.offset} with size: ${m.msgsize} in ${System.currentTimeMillis() - start} ms")
       sender ! KMessage(m.offset, m.timestamp, new String(decoded.bytes) + truncStr, m.size, decoder.getName(), decoded.size)
   }
 
