007dadff7740a97f7d984746b4d5098ded3412ae
owler <owler@tut.by>
2020-11-14 22:00:34 +0300

    test

--- src/test/scala/simple/SimpleMainTest.scala
+++ src/test/scala/simple/SimpleMainTest.scala
@@ -9,7 +9,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class SimpleMainTest extends AnyFunSuite {
   test("") {
     val conf = CharmConfigObject
-    println(conf.getString("env"))
+    assert(conf.getString("env") == "sit")
     assert(conf.parse("kafka")("ssl.truststore.password") == "encrypted password")
   }
   test("com.eclipse.Util.Password") {
