a5433321bf6225ca4c5f3b1ba476549941fbbee6
owler <owler@tut.by>
2021-01-16 19:24:28 +0300

    websso

--- src/main/scala/event/security/WebSSOHandler.scala
+++ src/main/scala/event/security/WebSSOHandler.scala
@@ -7,12 +7,9 @@ import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
 class WebSSOHandler(enabled: Boolean) extends AbstractHandler{
   override def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    println(request.getHeaderNames)
-    if(!check(baseRequest)){
-      if(!baseRequest.isHandled) {
+    if(enabled && !check(baseRequest)){
         response.sendError(HttpServletResponse.SC_FORBIDDEN)
         baseRequest.setHandled(true)
-      }
     }
   }
 
